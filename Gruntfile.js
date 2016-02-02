d0367bbedf55cc48206f299e144d262bb269f1a7
israt choudhury <israt_choudhury@dennis.co.uk>
2016-02-02 17:36:29 +0000

    hm; updating organised folders and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,100 @@
 module.exports = function(grunt) {
+	//1. All configuration goes here 
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-  // var adcfg = grunt.file.readJSON('adconfig.json');
-  // var awscfg = grunt.file.readJSON('awsInfo.json');
-  // var ftpcfg = grunt.file.readJSON('ftpInfo.json');
+		// concat: {
+		// 	// 2. Configuration for concatinating files goes here. (install it first)
 
-  // 1. All configuration goes here 
-  grunt.initConfig({
-    
-    pkg: grunt.file.readJSON('package.json'),
+		// }
 
+		//1. Using grunt-gm with graphicsmagick plugin to place concepts into a hipster frame
+		gm: {
+			mocks: {
+				options: {
+		        // default: false, check if dest file exists and size > 0 
+		        skipExisting: false,
+		        // default: false 
+		        stopOnError: false
+		    },
 
+				files: [
+					{
+						cwd: 'development/mocks',       // Src matches are relative to this path
+						dest: 'production/images',      // Destination path prefix
+						expand: true, 				    // Enable dynamic expansion
+						filter:'isFile',
+						src: ['**/**/*',  '!**/template/'],   //target files
+						options: {
+				            stopOnError: true
+			            },
+			            // image is passed as stream beteen tasks 
+			            tasks: [
+			            	{
+			            		// resize and crop image 
+				            	resize: [788],
+				            	crop:[788, 794, 0, 0,],
+				            }, 
+				            {
+				            	// extent and center the image with padding above it
+				            	gravity: ['South'],
+                                extent: [1160, 924],
+				            }, 
+				             {
+				              // FRAME IT-- add laptop 
+				              command: ['composite'],
+				              in: ['development/template/macbook.png'],
+				            }, {
+				              // adD reflection 
+				             command: ['composite'],
+				             in: ['development/template/shine.png'],
+				            }, {
+				              // final crop 
+				             crop:[1160, 794, 0, 0],
+				            }
 
+			            ]
+			            
+					}
 
-    /////////////////////////////////////////// GM mocks
-        
-    gm: {
-        mocks: {
-          options: {
-            // default: false, check if dest file exists and size > 0 
-            skipExisting: false,
-            // default: false 
-            stopOnError: false,
-            // task options will also be passed to arg callback 
-            yourcustomopt: {
-            //  'mocks/gruntjs.png': '"JavaScript Task Runner"',
-            //  'mocks/nodejs.png': '"JavaScript Runtime"'
-            }
-           // template : 'mocks/template/<%= myTask.src %>',
+				]
 
-          },
-          files: [
-            {
-              cwd: 'mocks',
-              dest: 'development/_images',
-              expand: true,
-              filter: 'isFile',
-              src: ['**/*', '!**/template/*'],
+			}
+		},
 
-              options: {
-               // skipExisting: true,
-                stopOnError: true
-              },
-              // image is passed as stream beteen tasks 
-              tasks: [
-                {
-                  // resize and crop image 
-                  resize: [788],
-                  crop:[788, 794, 0, 0],
-                }, {
-                  // extent and center the image with padding above it 
-                  gravity: ['South'],
-                  extent: [1160, 924],
-                }, {
-                  // add laptop 
-                  command: ['composite'],
-                  in: ['mocks/template/macbook.png'],
-                }, {
-                  // ad reflection 
-                 command: ['composite'],
-                 in: ['mocks/template/shine.png'],
-                }, {
-                  // final crop 
-                 crop:[1160, 794, 0, 0],
-                }
-              ]
-            }
-          ]
-        }
-      },
-
-      /////////////////////////////////////////// Minification
+	  //2. Minification of the images
       
-      imagemin: {
-        dynamic: {
-          files: [{
-            expand: true,
-            cwd: 'mocks/out/',
-            src: ['*.{png,jpg,gif}', '**/*.{png,jpg,gif}'],
-            dest: 'production/_images/'
-          }]
-        }
-      },
+	  imagemin: {
+	    dynamic: {
+	      files: [{
+	        expand: true,
+	        cwd: 'development/mocks/',							 // Src path for minification 
+	        src: ['*.{png,jpg,gif}', '*.{png,jpg,gif}'],		 // [file], [template frame]
+	        dest: 'production/images/'							 // Destination output
+	      }]
+	    }
+	  },
+
+	});
 
 
 
-    });
+
+	// 3. Where we tell Grunt we plan to use this plug-in. Install plugins with --save-dev
+
 
   
-
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-gm');
+    grunt.loadNpmTasks('grunt-gm'); //make sure install graphicsmagick inconjuction, info: https://www.npmjs.com/package/gm
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-sass');
 
 
 
-  /////////////////////////////////////////// default task when work is passed between developers or being worked on after again - Running grunt will not destroy previous work by overwriting from templates
-
+	// default task when work is passed between developers or being worked on after again 
+	//- Running grunt will not destroy previous work by overwriting from templates
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', [
-      'gm', 
-      'imagemin',
-    ]);
 
-
+    grunt.registerTask('default', ['imagemin', 'gm']);
 };
+
